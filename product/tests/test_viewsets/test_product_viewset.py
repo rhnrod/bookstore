186e958384c24080de180cd176fd280af4ed4101
Rhuan Emanuel <rhnrod@gmail.com>
2025-03-23 21:05:52 -0300

    adjust authentication and tests for order viewset

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -2,10 +2,8 @@ import json
 
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 
-from order.factories import UserFactory
 from product.factories import CategoryFactory, ProductFactory
 from product.models import Product
 
@@ -15,18 +13,12 @@ class TestProductViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
-        self.user = UserFactory()
-        token = Token.objects.create(user=self.user)
-        token.save()
-
         self.product = ProductFactory(
             title="pro controller",
             price=200.00,
         )
 
     def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -37,15 +29,12 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(product_data["results"][0]["active"], self.product.active)
 
     def test_create_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         category = CategoryFactory()
         data = json.dumps(
             {
                 "title": "notebook",
                 "price": 800.00,
                 "categories_id": [category.id],
-                "user": self.user.id,
             }
         )
 
